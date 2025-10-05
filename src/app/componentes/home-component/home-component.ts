d3198b8ef0a5658e79517f116afaf856eafe6234
thalitamariarocha <thalita.maria.rocha@gmail.com>
2025-10-05 20:29:15 -0300

    tela inicial ok

--- src/app/componentes/home-component/home-component.ts
+++ src/app/componentes/home-component/home-component.ts
@@ -1,34 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators , ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, Validators , ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-home-component',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './home-component.html',
   styleUrls: ['./home-component.css'],
   standalone: true,
 })
 export class HomeComponent {
 
-   form: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      uf: ['', Validators.required],
-      produto: ['', Validators.required]
-    });
-  }
+  empresa = '';
+  uf = '';
+  produto = '';
+  
+  formSubmitted = false;
 
   onSubmit() {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched(); // força exibição dos erros
-      return;
+    this.formSubmitted = true;
+
+    if (!this.uf || !this.produto || !this.empresa) {
+      return; // impede o envio se campos inválidos
     }
 
-    // TODO forma de consulta (redirecionar pag aqui)
-    console.log(this.form.value);
+    console.log('Consulta enviada:', { uf: this.uf, produto: this.produto });
   }
+
+
+  //  form: FormGroup;
+
+  // constructor(private fb: FormBuilder) {
+  //   this.form = this.fb.group({
+  //     uf: ['', Validators.required],
+  //     produto: ['', Validators.required],
+  //     empresa: ['', Validators.required]
+  //   });
+  // }
+
+  // onSubmit() {
+  //   if (this.form.invalid) {
+  //     this.form.markAllAsTouched(); // força exibição dos erros
+  //     return;
+  //   }
+
+  //   // TODO forma de consulta (redirecionar pag aqui)
+  //   console.log(this.form.value);
+  // }
 
 }
